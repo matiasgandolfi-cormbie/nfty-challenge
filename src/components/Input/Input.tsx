c209ca8c01218352d99845952169d531d657e45e
matiasgandolfi-cormbie <matias.gandolfi@crombie.dev>
2025-01-03 00:36:00 -0300

    fix error consola

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,5 @@
-'use client'
+'use client';
+
 import React from 'react';
 import { TextField } from '@mui/material';
 import { InputProps } from './types';
@@ -14,11 +15,19 @@ const Input: React.FC<InputProps> = ({
   onBlur,
 }) => {
   const maxDate = new Date().toISOString().split('T')[0];
+
+  // Corregir el formato para campos de tipo fecha
+  const formattedValue =
+    type === 'date' && typeof value === 'string'
+      ? value.split('T')[0]
+      : value;
+
   return (
     <>
       {title && (
         <label
           htmlFor={name}
+          id={`${name}-label`}
           className="block mb-1 text-sm font-medium text-gray-700"
         >
           {title} :
@@ -27,20 +36,22 @@ const Input: React.FC<InputProps> = ({
 
       <TextField
         id={name}
+        name={name}
         placeholder={label}
         type={type}
         variant="outlined"
         fullWidth
         margin="none"
         autoComplete="off"
-        value={value}
+        value={formattedValue}
         onChange={(e) =>
           onChange(type === 'number' ? +e.target.value : e.target.value)
         }
         onBlur={onBlur}
         error={!!error}
         helperText={error}
-        aria-label={title || label}
+        aria-label={label || title}
+        aria-labelledby={title ? `${name}-label` : undefined}
         aria-describedby={error ? `${name}-error` : undefined}
         role="textbox"
         {...(type === 'date' && {
